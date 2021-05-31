5f4a72be028de0b608aa744e3ba49dc37189239d
youxing99 <youxingpang@gmail.com>
2021-06-01 00:01:33 +0800

    Update train_mask_detector_incorrect.py

--- Face-Mask-Detector-main(3_cases)/Face-Mask-Detector-main/train_mask_detector_incorrect.py
+++ Face-Mask-Detector-main(3_cases)/Face-Mask-Detector-main/train_mask_detector_incorrect.py
@@ -168,7 +168,7 @@ model.save(args["model"], save_format="h5")
 
 
 mat = confusion_matrix(testY.argmax(axis=1), predIdxs)
-plot_confusion_matrix(conf_mat=mat, figsize=(8,8))
+print(plot_confusion_matrix(conf_mat=mat, figsize=(8,8)))
 
 
 
